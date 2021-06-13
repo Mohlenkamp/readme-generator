f6f7cad9d6b2ff0fdc84de0ad2470351e1651112
John Mohlenkamp <john@mohlenkamp.com>
2021-06-13 13:19:04 -0500

    finished questions

--- index.js
+++ index.js
@@ -1,13 +1,216 @@
 // TODO: Include packages needed for this application
+const fs = require('fs')  // File system 
+const inquirer = require('inquirer'); // Inquirer package
+const licenseArray = require('./utils/generateLicense.js');
+const { renderLicenseBadge, renderLicenseLink, renderLicenseSection, generateMarkdown } = require('./utils/generateMarkdown.js'); // Utility file
+
+const mockData = 
+{
+  developer: 'John Doe',
+  email: 'john.doe@gmail.com',
+  title: 'Generic Project Name',
+  projectDescription: 'This is a project description that I am putting in for demonstration purposes.\r\n' +
+    "It can be as long as you need. I'm going to use some lorem ipsum now to make it longer.\r\n" +
+    '\r\n' +
+    'Spicy jalapeno bacon ipsum dolor amet pork loin pig jerky, dolor pancetta minim bresaola buffalo reprehenderit ham hock flank. Corned beef tongue irure, cupim velit tail excepteur in ea ut hamburger. Flank ullamco beef nisi meatloaf occaecat buffalo meatball biltong id. Strip steak nulla doner qui labore beef ribs chuck drumstick picanha tongue bresaola exercitation dolore.\r\n' +
+    '\r\n' +
+    'Short ribs dolor reprehenderit, tongue nisi brisket salami ullamco id duis elit cupidatat doner. Mollit beef burgdoggen capicola chuck dolor. Tongue ex meatball ullamco chislic frankfurter anim venison ribeye exercitation swine buffalo short ribs pastrami sint. Jowl dolore esse biltong filet mignon capicola. Flank proident veniam ham in, shank et jerky shankle duis pork belly \r\n' +
+    'anim chislic occaecat filet mignon. Ullamco bacon buffalo tongue tempor hamburger est dolore jowl shankle mollit t-bone pancetta. Pastrami t-bone pork loin, esse cow magna pork belly minim salami deserunt eiusmod exercitation lorem.\r\n' +
+    '\r\n' +
+    'Officia lorem ball tip deserunt leberkas biltong, corned beef boudin. Do eiusmod ham deserunt ham hock andouille dolore short ribs. Veniam prosciutto eu excepteur dolore et doner. Bresaola cupim filet mignon ground round enim dolore turkey qui incididunt in short loin. Fatback magna alcatra laboris meatloaf pariatur hamburger cupim tail pork loin. Sed short ribs laborum corned beef pork belly cow esse qui chicken in quis meatball prosciutto jowl aute.',
+  githubName: 'johndoeGit',
+  gitHubLink: 'johndoeGit@github.com',
+  installation: 'This is used to put in any specific instructions you may want.\r\n' +
+    '\r\n' +
+    'Spicy jalapeno bacon ipsum dolor amet pork loin pig jerky, dolor pancetta minim bresaola buffalo reprehenderit ham hock flank. Corned beef tongue irure, cupim velit tail excepteur in ea ut hamburger. Flank ullamco beef nisi meatloaf occaecat buffalo meatball biltong id. Strip steak nulla doner qui labore beef ribs chuck drumstick picanha tongue bresaola exercitation dolore.\r\n' +
+    '\r\n' +
+    'Short ribs dolor reprehenderit, tongue nisi brisket salami ullamco id duis elit cupidatat doner. Mollit beef burgdoggen capicola chuck dolor. Tongue ex meatball ullamco chislic frankfurter anim venison ribeye exercitation swine buffalo short ribs pastrami sint. Jowl dolore esse biltong filet mignon capicola. Flank proident veniam ham in, shank et jerky shankle duis pork belly \r\n' +
+    'anim chislic occaecat filet mignon. Ullamco bacon buffalo tongue tempor hamburger est dolore jowl shankle mollit t-bone pancetta. Pastrami t-bone pork loin, esse cow magna pork belly minim salami deserunt eiusmod exercitation lorem.\r\n' +
+    '\r\n' +
+    'Officia lorem ball tip deserunt leberkas biltong, corned beef boudin. Do eiusmod ham deserunt ham hock andouille dolore short ribs. Veniam prosciutto eu excepteur dolore et doner. Bresaola cupim filet mignon ground round enim dolore turkey qui incididunt in short loin. Fatback magna alcatra laboris meatloaf pariatur hamburger cupim tail pork loin. Sed short ribs laborum corned beef pork belly cow esse qui chicken in quis meatball prosciutto jowl aute.',
+  confirmUsage: true,
+  usageInstructions: 'none',
+  confirmLicense: true,
+  license: [ 'Do What The F*ck You Want To Public License' ],
+  badgeColor: [ 'blueviolet' ]
+}
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'developer',
+    message: 'What is your name? (Required)',
+    validate: developerName => {
+      if (developerName) {
+        return true;
+      } else {
+        console.log('You need to enter your name!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address? (Required)',
+    validate: developerEmail => {
+      if (developerEmail) {
+        return true;
+      } else {
+        console.log('You need to enter your email address!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the name of your project? (Required)',
+    validate: projectName => {
+      if (projectName) {
+        return true;
+      } else {
+        console.log('You need to enter a project name!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'editor',
+    name: 'projectDescription',
+    message: 'Provide a description of the project (Required/Use Editor)',
+    validate: descriptionInput => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log('You need to enter a project description!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'githubName',
+    message: 'What is your GitHub user name? (Required)',
+    validate: developerName => {
+      if (developerName) {
+        return true;
+      } else {
+        console.log('You need to enter your user name!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'gitHubLink',
+    message: 'Enter the GitHub link to your project. (Required)',
+    validate: linkInput => {
+      if (linkInput) {
+        return true;
+      } else {
+        console.log('You need to enter a project GitHub link!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'editor',
+    name: 'installation',
+    message: 'Provide any specific installation instructions (Required/Use Editor)',
+    validate: installInput => {
+      if (installInput) {
+        return true;
+      } else {
+        console.log('You need to enter some installation instructions!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'confirm',
+    name: 'confirmUsage',
+    message: 'Does you have any usage instructions or screenshots?',
+    default: false
+  },
+  {
+    type: 'Editor',
+    name: 'usageInstructions',
+    message: 'What are your usage instructions (Use Editor)?',
+    when: ({ confirmUsage }) => confirmUsage
+  },
+  {
+    type: 'confirm',
+    name: 'confirmLicense',
+    message: 'Does your project have a license associated with it?',
+    default: false
+  },
+  {
+    type: 'checkbox',
+    name: 'license',
+    message: 'What license is associated with this project? (Check ONLY one)',
+    choices: licenseArray,
+    when: ({ confirmLicense }) => confirmLicense
+  },
+  {
+    type: 'checkbox',
+    name: 'badgeColor',
+    message: 'Choose your badge color please(Check ONLY one)',
+    choices: ['brightgreen','green','yellowgreen','yellow','orange','red','blue','lightgrey','blueviolet','ff69b4','9cf'],
+    default: 'lightgrey',
+    when: ({ confirmLicense }) => confirmLicense
+  }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = fileContent => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/README.md', fileContent, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: 'README.md file created'
+      });
+    });
+  });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
+
+function init() {
+  inquirer.prompt(questions)
+      .then(function (data) {
+        console.log(data)
+        console.log(data.developer)
+        console.log(data.title)
+      })
+  }
+
+// .then(askAboutMock => {console.log("Mock data use: " + askAboutMock)
+//   return askQuestions();
+// })
+// .then(askQuestions
+// .then(portfolioData => {
+//   return generatePage(portfolioData);
+// })
+// .then(pageHTML => {
+//   return writeFile(pageHTML);
+// })
+// .then(writeFileResponse => {
+//   console.log(writeFileResponse);
+//   return copyFile();
+// })
+// .then(copyFileResponse => {
+//   console.log(copyFileResponse);
+// })
+
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,53 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+
+const licenseArray = require("./generateLicense");
+
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license, badgeColor) {
+  if (!badgeColor){badgeColor="9cf"} //default color if none chosen
+  switch (license){
+    case "Academic Free License v3.0" : return (`<img src="https://img.shields.io/badge/license-afl--3.0-${badgeColor}" alt="license badge"`);break;
+    case "Apache license 2.0" : return (`<img src="https://img.shields.io/badge/license-apache--2.0-${badgeColor}" alt="license badge"`)
+    case "Artistic license 2.0" : return (`<img src="https://img.shields.io/badge/license-artistic--2.0-${badgeColor}" alt="license badge"`)
+    case "Boost Software License 1.0" : return (`<img src="https://img.shields.io/badge/license-bsl--1.0--2.0-${badgeColor}" alt="license badge"`)
+    case "BSD 2-clause Simplified license" : return (`<img src="https://img.shields.io/badge/license-bsd--2--clause-${badgeColor}" alt="license badge"`)
+    case "BSD 3-clause New or Revised license" : return (`<img src="https://img.shields.io/badge/license-bsd--3--clause-${badgeColor}" alt="license badge"`)
+    case "BSD 3-clause Clear license" : return (`<img src="https://img.shields.io/badge/license-bsd--3--clause--clear-${badgeColor}" alt="license badge"`)
+    case "Creative Commons license family" : return (`<img src="https://img.shields.io/badge/license-cc-${badgeColor}" alt="license badge"`)
+    case "Creative Commons Zero v1.0 Universal" : return (`<img src="https://img.shields.io/badge/license-cc0--1.0-${badgeColor}" alt="license badge"`)
+    case "Creative Commons Attribution 4.0" : return (`<img src="https://img.shields.io/badge/license-cc--by--4.0-${badgeColor}" alt="license badge"`)
+    case "Creative Commons Attribution Share Alike 4.0" : return (`<img src="https://img.shields.io/badge/license-cc--by--sa--4.0-${badgeColor}" alt="license badge"`)
+    case "Do What The F*ck You Want To Public License" : return (`<img src="https://img.shields.io/badge/license-wtfpl-${badgeColor}" alt="license badge"`)
+    case "Educational Community License v2.0" : return (`<img src="https://img.shields.io/badge/license-ecl--2.0-${badgeColor}" alt="license badge"`)
+    case "Eclipse Public License 1.0" : return (`<img src="https://img.shields.io/badge/license-epl--1.0-${badgeColor}" alt="license badge"`)
+    case "Eclipse Public License 2.0" : return (`<img src="https://img.shields.io/badge/license-epl--2.0-${badgeColor}" alt="license badge"`)
+    case "European Union Public License 1.1" : return (`<img src="https://img.shields.io/badge/license-eupl--1.1-${badgeColor}" alt="license badge"`)
+    case "GNU Affero General Public License v3.0" : return (`<img src="https://img.shields.io/badge/license-agpl--3.0-${badgeColor}" alt="license badge"`)
+    case "GNU General Public License family" : return (`<img src="https://img.shields.io/badge/license-gpl-${badgeColor}" alt="license badge"`)
+    case "GNU General Public License v2.0" : return (`<img src="https://img.shields.io/badge/license-gpl--2.0-${badgeColor}" alt="license badge"`)
+    case "GNU General Public License v3.0" : return (`<img src="https://img.shields.io/badge/license-gpl--3.0-${badgeColor}" alt="license badge"`)
+    case "GNU Lesser General Public License family" : return (`<img src="https://img.shields.io/badge/license-lgpl-${badgeColor}" alt="license badge"`)
+    case "GNU Lesser General Public License v2.1" : return (`<img src="https://img.shields.io/badge/license-lgpl--2.1-${badgeColor}" alt="license badge"`)
+    case "GNU Lesser General Public License v3.0" : return (`<img src="https://img.shields.io/badge/license-lgpl--3.0-${badgeColor}" alt="license badge"`)
+    case "ISC" : return (`<img src="https://img.shields.io/badge/license-isc-${badgeColor}" alt="license badge"`)
+    case "LaTeX Project Public License v1.3c" : return (`<img src="https://img.shields.io/badge/license-lppl--1.3c-${badgeColor}" alt="license badge"`)
+    case "Microsoft Public License" : return (`<img src="https://img.shields.io/badge/license-ms--pl-${badgeColor}" alt="license badge"`)
+    case "MIT" : return (`<img src="https://img.shields.io/badge/license-mit-${badgeColor}" alt="license badge"`)
+    case "Mozilla Public License 2.0" : return (`<img src="https://img.shields.io/badge/license-mpl--2.0-${badgeColor}" alt="license badge"`)
+    case "Open Software License 3.0" : return (`<img src="https://img.shields.io/badge/license-osl--3.0-${badgeColor}" alt="license badge"`)
+    case "PostgreSQL License" : return (`<img src="https://img.shields.io/badge/license-postgresql-${badgeColor}" alt="license badge"`)
+    case "SIL Open Font License 1.1" : return (`<img src="https://img.shields.io/badge/license-ofl--1.1-${badgeColor}" alt="license badge"`)
+    case "University of Illinois/NCSA Open Source License" : return (`<img src="https://img.shields.io/badge/license-ncsa-${badgeColor}" alt="license badge"`)
+    case "The Unlicense" : return (`<img src="https://img.shields.io/badge/license-unlicense-${badgeColor}" alt="license badge"`)
+    case "zLib License" : return (`<img src="https://img.shields.io/badge/license-zlib-${badgeColor}" alt="license badge"`)
+    default : return (`<img src="https://img.shields.io/badge/license-unknown-${badgeColor}" alt="license badge"`)
+  } 
+          
+  //https://img.shields.io/badge/license-afl--3.0-lightgrey
+
+   
+//return `https://img.shields.io/badge/License--$(license}--green`)
+//return `<img src="https://img.shields.io/badge/License-$(license}--green" alt="license badge"`)
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,7 +59,56 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
+${renderLicenseBadge(data.license, data.badgeColor)}
+## Description 
+
+${data.projectDescription}
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+${renderLicenseLink(data.license)}
+* [Features](#features)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usageInstructions}
+
+![alt text](assets/images/screenshot.png)
+
+## Credits
+
+${data.credits}
+
+${renderLicenseSection(data.license)}
+## Features
+
+${data.features}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+${data.developer}
+${data.email}
 
 `;
 }
